ad8410b4525f7b8d216bd730cbd707fb3019a9ae
Matthew Flatt <mflatt@racket-lang.org>
2022-09-21 05:22:55 +0800

    test script fixup

--- .github/workflows/test.sh
+++ .github/workflows/test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export ZUO_JOBS="$(getconf _NPROCESSORS_ONLN)"
 if test -n "$USE_MSVC" ; then
-    ./build.bat $TARGET_MACHINE /test-some
+    ./build.bat $TARGET_MACHINE "/test-some"
 else
     make test-some
 fi
