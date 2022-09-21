d828f3d564d34fa7fae8616f8eb84e7ef64daf2b
Matthew Flatt <mflatt@racket-lang.org>
2022-09-21 10:57:33 +0800

    test-one for pb

--- .github/workflows/test.sh
+++ .github/workflows/test.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 export ZUO_JOBS="$(getconf _NPROCESSORS_ONLN)"
+if test "$TEST_TARGET" = ""; then
+    TEST_TARGET=test-some
+fi
 if test "$TOOLCHAIN" = vs ; then
-    cmd.exe /c "build.bat $TARGET_MACHINE /test-some"
+    cmd.exe /c "build.bat $TARGET_MACHINE /$TEST_TARGET"
 else
-    make test-some
+    make $TEST_TARGET
 fi
